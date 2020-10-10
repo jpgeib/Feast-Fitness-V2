4e9ada0ce2954a063be375d68143f0feac2f1a81
James Geib <jpgeib2@gmail.com>
2020-10-10 18:30:03 -0400

    created folder structure for MealSearchForm in components

---
+++ client/src/components/MealSearchForm/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Grid, Form, Button, Header, Dropdown } from "semantic-ui-react";
+import { Field, reduxform } from "redux-form";
+import { required } from "redux-form-validators";
+
+import "./style.css";
+
+class MealFormSearch extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default MealFormSearch;
