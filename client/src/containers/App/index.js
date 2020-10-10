73084685c8524ffe32de6f37b0295e0b3c7ec40c
James Geib <jpgeib2@gmail.com>
2020-10-10 13:50:22 -0400

    implemented Routes for each page in App container, also added Router tags around App container render in main index.js of src

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from "react-router-dom";
+import Home from "../../pages/Home";
+import MealPlanner from "../../pages/MealPlanner";
+import BMI from "../../pages/BMI";
 
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/mealplanner" component={MealPlanner} />
+                <Route exact path="bmi" component={BMI} />
+            </>
+        );
+
+    }
 }
 
 export default App;
