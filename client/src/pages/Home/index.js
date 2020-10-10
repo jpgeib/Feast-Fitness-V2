1b7e42715491319d38aa8b9416436f43e652ad87
James Geib <jpgeib2@gmail.com>
2020-10-10 15:17:50 -0400

    constructed initial Grid structure for Home page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,16 +1,64 @@
 import React, { Component } from "react";
-import { Grid, Header } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Grid, Header, Image, Button } from "semantic-ui-react";
+import Silverware from "../../assets/images/fork-spoon.png";
+import HeartRate from "../../assets/images/heart-rate.png"
 
 import "./style.css";
 
 class Home extends Component {
     render() {
-        return(
-            <Grid>
-                <Grid.Column>
-                    <Header as="h1">Home</Header>
-                </Grid.Column>
-            </Grid>
+        return (
+            <div id="home-page">
+                <Grid id="home-container">
+                    <Grid.Row centered>
+                        <Grid.Column width={16}>
+                            <Header as="h1">Welcome to Feast and Fitness</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={Silverware} size="medium" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">Eat</Header>
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">Choose a healthy meal option that suits your dietary needs from our recipe search engine!</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <Image src={HeartRate} size="medium" />
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header as="h2">Watch What You Eat</Header>
+                                    </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Header as="h3">Then, to make sure you're meeting your dietary needs, track your meals and caloric intake with our calorie tracker!</Header>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Button
+                            as={Link}
+                            to="/mealplanner" 
+                            content="Not sure where to start? Click here!"
+                            id="home-btn" 
+                            size="large" 
+                            color="blue" />
+                    </Grid.Row>
+                </Grid>
+            </div>
         )
     }
 }
