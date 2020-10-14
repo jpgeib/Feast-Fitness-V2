36b7907d9e2d2e90f6aa477e4d8787415edbc500
James Geib <jpgeib2@gmail.com>
2020-10-13 21:28:15 -0400

    constructed CalorieLogCard component

--- client/src/components/CalorieLogCard/index.js
+++ client/src/components/CalorieLogCard/index.js
@@ -4,5 +4,48 @@ import { Card, Input, Button, Header } from "semantic-ui-react";
 import "./style.css";
 
 class CalorieLogCard extends Component {
-    
+    render() {
+        return (
+            <Card>
+                <Card.Content>
+                    <Card.Header>Breakfast</Card.Header>
+                    <Card.Description>
+                        <Input label="Food" />
+                        <Input label="Calories" />
+                        <Button content="Save" size="small" color="blue" />
+                        <Button content="clear" size="small" color="blue" />
+                    </Card.Description>
+                    <Card.Header>Lunch</Card.Header>
+                    <Card.Description>
+                        <Input label="Food" />
+                        <Input label="Calories" />
+                        <Button content="Save" size="small" color="blue" />
+                        <Button content="clear" size="small" color="blue" />
+                    </Card.Description>
+                    <Card.Header>Dinner</Card.Header>
+                    <Card.Description>
+                        <Input label="Food" />
+                        <Input label="Calories" />
+                        <Button content="Save" size="small" color="blue" />
+                        <Button content="clear" size="small" color="blue" />
+                    </Card.Description>
+                    <Card.Header>Snacks</Card.Header>
+                    <Card.Description>
+                        <Input label="Food" />
+                        <Input label="Calories" />
+                        <Button content="Save" size="small" color="blue" />
+                        <Button content="clear" size="small" color="blue" />
+                    </Card.Description>
+                    <Card.Header>Exercise</Card.Header>
+                    <Card.Description>
+                        <Input label="Workout" />
+                        <Input label="Calories Burnt" />
+                        <Button content="Save" size="small" color="blue" />
+                        <Button content="clear" size="small" color="blue" />
+                    </Card.Description>
+                </Card.Content>
+            </Card>
+        );
+    }
+
 }
