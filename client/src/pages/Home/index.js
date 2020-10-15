bde106014f3c82cc4d05b28de77e15a0d3fd54f7
James Geib <jpgeib2@gmail.com>
2020-10-14 20:47:24 -0400

    changed color of Home page images to light green color, added vegetable image as placeholder backdrop for welcoming section

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Grid, Header, Image, Button } from "semantic-ui-react";
 import Silverware from "../../assets/images/fork-spoon.png";
 import HeartRate from "../../assets/images/heart-rate.png"
+import Vegetables from "../../assets/images/alt-veggie-backdrop1-edited.jpg";
 
 import "./style.css";
 
@@ -11,50 +12,65 @@ class Home extends Component {
         return (
             <div id="home-page">
                 <Grid id="home-container">
-                    <Grid.Row centered>
-                        <Grid.Column width={16}>
-                            <Header as="h1">Welcome to Feast and Fitness</Header>
-                        </Grid.Column>
+                    <Grid.Row>
+                        <Grid id="home-image-container">
+                            <Grid.Row centered>
+                                <Grid.Column width={16}>
+                                    <Header as="h1">Welcome to Feast and Fitness</Header>
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={16}>
+                                    <Image src={Vegetables} size="huge" />
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Image src={Silverware} size="medium" />
-                                    </Grid.Column>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Eat</Header>
-                                    </Grid.Column>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">Choose a healthy meal option that suits your dietary needs from our recipe search engine!</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column width={16}>
-                                        <Image src={HeartRate} size="medium" />
-                                    </Grid.Column>
-                                    <Grid.Column width={16}>
-                                        <Header as="h2">Watch What You Eat</Header>
-                                    </Grid.Column>
-                                    <Grid.Column width={16}>
-                                        <Header as="h3">Then, to make sure you're meeting your dietary needs, track your meals and caloric intake with our calorie tracker!</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
+                        <Grid>
+                            <Grid.Row>
+                                <Grid.Column width={8}>
+                                    <Grid>
+                                        <Grid.Row>
+                                            <Grid.Column width={16}>
+                                                <Image src={Silverware} size="medium" />
+                                            </Grid.Column>
+                                            <Grid.Column width={16}>
+                                                <Header as="h2">Eat</Header>
+                                            </Grid.Column>
+                                            <Grid.Column width={16}>
+                                                <Header as="h3">Choose a healthy meal option that suits your dietary needs from our recipe search engine!</Header>
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                    </Grid>
+                                </Grid.Column>
+                                <Grid.Column width={8}>
+                                    <Grid>
+                                        <Grid.Row>
+                                            <Grid.Column width={16}>
+                                                <Image src={HeartRate} size="medium" />
+                                            </Grid.Column>
+                                            <Grid.Column width={16}>
+                                                <Header as="h2">Watch What You Eat</Header>
+                                            </Grid.Column>
+                                            <Grid.Column width={16}>
+                                                <Header as="h3">Then, to make sure you're meeting your dietary needs, track your meals and caloric intake with our calorie tracker!</Header>
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                    </Grid>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
                     </Grid.Row>
+
+
                     <Grid.Row>
                         <Button
                             as={Link}
-                            to="/mealplanner" 
+                            to="/mealplanner"
                             content="Not sure where to start? Click here!"
-                            id="home-btn" 
-                            size="large" 
+                            id="home-btn"
+                            size="large"
                             color="blue" />
                     </Grid.Row>
                 </Grid>
